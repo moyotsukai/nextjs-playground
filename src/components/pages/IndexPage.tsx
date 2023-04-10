6e42eda0839d85818fa7f39f24074d20b906f94a
moyotsukai <gyoshu.bowie@gmail.com>
2023-04-11 02:39:31 +0900

    add: react-hook-form

--- src/components/pages/IndexPage.tsx
+++ src/components/pages/IndexPage.tsx
@@ -1,7 +1,49 @@
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type FormInput = {
+  name: string,
+  mail: string,
+  gender: "male" | "female" | "other"
+}
+
 export default function IndexPage() {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm<FormInput>()
+
+  const onSubmit: SubmitHandler<FormInput> = (data) => {
+    console.log(data)
+  }
+
+  console.log(watch("name"))
+
   return (
-    <div>
-      Hello!
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>Name</label>
+      <input
+        defaultValue="test"
+        {...register("name", { required: true })}
+        aria-invalid={errors.name ? "true" : "false"}
+      />
+      {errors.name?.type === "required" && (
+        <p role="alert">Name is required</p>
+      )}
+
+      <label>Mail</label>
+      <input
+        {...register("mail", { required: "Email is requied" })}
+        aria-invalid={errors.mail ? "true" : "false"}
+      />
+      {errors.mail && (
+        <p role="alert">{errors.mail?.message}</p>
+      )}
+
+      <label>Gender</label>
+      <select {...register("gender")}>
+        <option value="male">male</option>
+        <option value="female">female</option>
+        <option value="other">other</option>
+      </select>
+
+      <input type="submit" />
+    </form>
   )
 }
