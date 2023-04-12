5760f4c12298630611df2716463161ac6f8c0d7a
moyotsukai <gyoshu.bowie@gmail.com>
2023-04-12 14:36:35 +0900

    add: form with zod

--- src/components/form/FormWithZod.tsx
+++ src/components/form/FormWithZod.tsx
@@ -2,10 +2,20 @@ import { z } from "zod"
 import { SubmitHandler, useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 
-const userSchema = z.object({
-  name: z.string().min(1, { message: "Required" }),
-  age: z.number().min(10)
-})
+const userSchema = z
+  .object({
+    firstName: z.string().min(1, { message: "Required" }),
+    familyName: z.string().min(1, { message: "Required" }),
+    age: z.number().min(10)
+  })
+  .transform((data) => {
+    const fullName = `${data.firstName} ${data.familyName}`
+
+    return {
+      ...data,
+      fullName
+    }
+  })
 
 type User = z.infer<typeof userSchema>
 
@@ -18,11 +28,19 @@ export default function FormWithZod() {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("name")} />
-      {errors.name && (
-        <p>{errors.name?.message}</p>
+      <label>First name</label>
+      <input {...register("firstName")} />
+      {errors.firstName && (
+        <p>{errors.firstName?.message}</p>
       )}
 
+      <label>Family name</label>
+      <input {...register("familyName")} />
+      {errors.familyName && (
+        <p>{errors.familyName?.message}</p>
+      )}
+
+      <label>Age</label>
       <input type="number" {...register("age", { valueAsNumber: true })} />
       {errors.age &&
         <p>{errors.age?.message}</p>
