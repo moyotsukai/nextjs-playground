c532914ef9d3f91916f819b89cd2d2b57638f46b
moyotsukai <gyoshu.bowie@gmail.com>
2023-04-12 13:43:21 +0900

    add

--- src/components/pages/IndexPage.tsx
+++ src/components/pages/IndexPage.tsx
@@ -1,4 +1,15 @@
+import { z } from "zod"
+
+const User = z.object({
+  username: z.string(),
+})
+
+type User = z.infer<typeof User>
+
 export default function IndexPage() {
+  const parsed = User.parse({ username: "Ludwig" })
+  console.log(parsed)
+
   return (
     <div>
       Hello!
